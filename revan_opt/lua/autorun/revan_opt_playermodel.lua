4675af3c2c2eab38921974fa42bd49fbb936fe92
QuarianDude <91162702+QuarianDude@users.noreply.github.com>
2025-09-27 00:57:56 +0100

    Add files via upload

---
+++ revan_opt/lua/autorun/revan_opt_playermodel.lua
@@ -0,0 +1,2 @@
+player_manager.AddValidModel( "revan_opt", "models/epangelmatikes/revan/revan_opt.mdl" )
+player_manager.AddValidHands( "revan_opt", "models/epangelmatikes/revan/revan_hands_opt.mdl", 0, "00000000" )
